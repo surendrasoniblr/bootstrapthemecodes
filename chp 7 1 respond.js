b32a3b46f4015f08e8cf8c805e25c871d389f0bb
Surendra Soni <surendra.sonit@gmail.com>
2015-05-30 13:36:21 +0530

    Create chp 7 1 respond.js

---
+++ chp 7 1 respond.js
@@ -0,0 +1,19 @@
+/**
+ * Enqueue scripts and styles.
+ */
+function bootstrapwp_scripts() {
+	wp_enqueue_style( 'bootstrap-styles', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.2.0', 'all' );
+ 
+	wp_enqueue_style( 'bootstrapwp-style', get_stylesheet_uri() );
+ 
+	wp_enqueue_script( 'respond-js', get_template_directory_uri() . '/js/respond.min.js', array('jquery'), '1.4.2', true );
+ 
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.2.0', true );
+ 
+	wp_enqueue_script( 'bootstrapwp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+ 
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'bootstrapwp_scripts' );
