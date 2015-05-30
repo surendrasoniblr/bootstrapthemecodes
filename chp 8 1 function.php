ebd05d8f755c81181789c8a76b19f6f596bc3b0f
Surendra Soni <surendra.sonit@gmail.com>
2015-05-30 13:41:49 +0530

    Create chp 8 1 function.php

---
+++ chp 8 1 function.php
@@ -0,0 +1,14 @@
+/**
+ * Add Respond.js for IE
+ */
+if( !function_exists('ie_scripts')) {
+	function ie_scripts() {
+	 	echo '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->';
+	   	echo ' <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->';
+	   	echo ' <!--[if lt IE 9]>';
+	    echo ' <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>';
+	    echo ' <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
+	   	echo ' <![endif]-->';
+   	}
+   	add_action('wp_head', 'ie_scripts');
+} // end if
